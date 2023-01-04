ce3da149d211bc6389fc7cb870e909c547d116bf
Efe Cavdar <cavdar.efe@outlook.com>
2023-01-04 19:29:24 +0300

    HTML file created and all process tested

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return render_template("")
+    return render_template('index.html')
 
 @app.route("/predict", methods = ["POST"])
 def predict():
@@ -17,9 +17,9 @@ def predict():
     scores = [np.array(int_scores)]
     prediction = model.predict(scores)
     
-    output = 100 * round(prediction[0], 2)
+    output = 100 * prediction[0]
     
-    return render_template("index.html", prediction_text = "Percent of Admission is {}".format(output))
+    return render_template('index.html', prediction_text = "Percent of Admission is %{}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
